c59384f40df97d449e21e49b1cceac614618cf28
Timothy Mei <timothymei3270@gmail.com>
2023-02-16 19:34:16 -0800

    optimized header and login styles for mobile

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
             d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
           />
         </svg>
-        <span className="font-medium text-2xl">windbnb</span>
+        <span className="font-medium text-2xl hidden sm:flex">windbnb</span>
       </a>
       <div className="flex items-center text-sm gap-3 border border-gray-300 rounded-full py-2 px-6 shadow-md shadow-gray-300">
         <div className="font-medium">Anywhere</div>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 const Login = () => {
   return(
-    <div className="mt-10 border border-gray-400 rounded-xl max-w-[80%] mx-auto p-9">
+    <div className="mt-10 border border-gray-400 rounded-xl sm:max-w-md max-w-[80%] mx-auto p-9">
       <h1 className="text-md font-semibold text-center border-b-2 pb-3 mb-4">Log in</h1>
       <h1 className="text-2xl font-medium mb-2">Welcome back to Windbnb</h1>
       <form>
