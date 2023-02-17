3cc347af191bc546a882adb581fa2a285defb5b6
Timothy Mei <timothymei3270@gmail.com>
2023-02-16 16:03:09 -0800

    seperated header to its own component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Login from './pages/Login'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/login" element={<Login />}></Route>
     </Routes>
   )
 }
