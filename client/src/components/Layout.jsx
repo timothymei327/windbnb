4862157c23c141590baf31007350a214204b8945
Timothy Mei <timothymei3270@gmail.com>
2023-03-24 12:34:05 -0400

    fixed styling

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -3,9 +3,9 @@ import {Outlet} from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div className="flex p-2 flex-col justify-evenly items-stretch">
+    <div className="flex py-4 px-2 flex-col justify-evenly items-stretch">
       <Header />
-      <div className="py-4 px-8 flex flex-col min-h-screen max-w-4xl mx-auto">
+      <div className="py-4 px-16 flex flex-col min-h-screen max-w-4xl mx-auto">
         <Outlet />
       </div>
     </div>
